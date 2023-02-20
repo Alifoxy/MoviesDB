2911eaaf531cdcbaffa5bd1b715d10e642f5c1b3
Alifoxy <nightfoxy666@gmail.com>
2023-02-20 16:56:57 +0300

    comm1

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -1,12 +1,7 @@
-const baseURL = 'https://api.themoviedb.org/3/movie/movie_id?api_key=cb1f2b7412e28a08ff00da197875b432'
-
-const movies = '/movies'
+const baseURL = 'https://api.themoviedb.org/3/movie/140?api_key=cb1f2b7412e28a08ff00da197875b432'
 
 const urls = {
-    movies: {
-        movies,
-        byId: (movie_id) => `${movies}/${movie_id}`
-    }
+    movies: '/movies'
 }
 
 export {
