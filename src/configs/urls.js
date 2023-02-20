9961c7081eaffe9430d12aace207937cf1a1c1c9
Alifoxy <nightfoxy666@gmail.com>
2023-02-20 17:03:00 +0300

    comm1

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -1,8 +1,14 @@
 const baseURL = 'https://api.themoviedb.org/3/movie/140?api_key=cb1f2b7412e28a08ff00da197875b432'
 
+const movies = '/movies'
+
 const urls = {
-    movies: '/movies'
+    movies: {
+        movies,
+        byId: (id) => `${movies}/${id}`
+    }
 }
+
 
 export {
     baseURL,
