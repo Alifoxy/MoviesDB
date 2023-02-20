dd08e51b1c58308c60237e3c5ac9c235038bc0ee
Alifoxy <nightfoxy666@gmail.com>
2023-02-20 21:38:17 +0300

    comm1

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,19 +1,25 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
+import {useSearchParams} from "react-router-dom";
 
 import {moviesActions} from "../../redux";
 import {Movie} from "./MoviesListCard";
 
 const Movies = () => {
-    const {movies} = useSelector(state => state.movies);
+    const {movies, prev, next} = useSelector(state => state.movies);
     const dispatch = useDispatch();
+    const [query, setQuery] = useSearchParams({page:'1'});
 
     useEffect(() => {
-        dispatch(moviesActions.getAll())
-    }, [dispatch])
+        dispatch(moviesActions.getAll({page:query.get('page')}))
+    }, [dispatch, query])
 
     return (
         <div>
+            <div>
+                <button disabled={!prev} onClick={()=>setQuery(query=>({page:+query.get('page')-1}))}>prev</button>
+                <button disabled={!next} onClick={()=>setQuery(query=>({page:+query.get('page')+1}))}>next</button>
+            </div>
             {movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
         </div>
     );
