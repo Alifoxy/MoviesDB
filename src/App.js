2b9a20c45e3e32c27db6978341efe98eb9990988
Alifoxy <nightfoxy666@gmail.com>
2023-02-20 17:45:36 +0300

    comm1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import MainLayout from "./layouts/MainLayout";
-import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-            <Route path={'/'} element={<MainLayout/>}>
-            </Route>
-        </Routes>
+        <MainLayout/>
     </div>
   );
 }
