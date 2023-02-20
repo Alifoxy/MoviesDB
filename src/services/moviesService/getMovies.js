141e0109b4b95ad8ae66f8b0181eb8f6508a28aa
Alifoxy <nightfoxy666@gmail.com>
2023-02-20 16:40:51 +0300

    comm1

--- src/services/moviesService/getMovies.js
+++ src/services/moviesService/getMovies.js
@@ -4,7 +4,7 @@ import {urls} from "../../configs/urls";
 
 const getMovies = {
     getAll: () => axiosRequest.get(urls.movies.movies),
-    getById: (id) => axiosRequest.get(`${urls.movies.movies}/${id}`)
+    getById: (movie_id) => axiosRequest.get(`${urls.movies.movies}/${movie_id}`)
 }
 
 export {
