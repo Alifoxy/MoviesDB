b6bb9acaa1fb3d51f966354a73e9821ddf5e7d7f
Alifoxy <nightfoxy666@gmail.com>
2023-02-20 17:44:05 +0300

    comm1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import MainLayout from "./layouts/MainLayout";
-import {Navigate, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
         <Routes>
             <Route path={'/'} element={<MainLayout/>}>
-                <Route index element={<Navigate to={'cars'}/>}/>
             </Route>
         </Routes>
     </div>
