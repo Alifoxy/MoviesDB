52a879c92bcb83f3efbe7ec340be7277ff57cedf
Alifoxy <nightfoxy666@gmail.com>
2023-02-21 19:24:55 +0300

    comm1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import {Navigate, Route, Routes} from "react-router-dom";
 
-import {MainLayout} from "./layouts/MainLayout";
+import {AuthRequireLayout, MainLayout} from "./layouts";
 
-import {MoviesPage} from "./containers/MoviesPage/MoviesPage";
+import {MoviesPage,RegisterPage,LoginPage} from "./containers";
 
 const App = () => {
     return(
         <Routes>
             <Route path={'/'} element={<MainLayout/>}>
                 <Route index element={<Navigate to={'movies'}/>}/>
+
+                <Route element={<AuthRequireLayout/>}>
                     <Route path={'movies'} element={<MoviesPage/>}/>
+                </Route>
+
+                <Route path={'login'} element={<LoginPage/>}/>
+                <Route path={'register'} element={<RegisterPage/>}/>
 
             </Route>
         </Routes>
--- src/services/moviesService/getMovies.js
+++ src/services/moviesService/getMovies.js
@@ -5,7 +5,7 @@ import {urls} from "../../configs/urls";
 const getMovies = {
     getAll: (page=1) => apiService.get(urls.movies.movies, {params:{page}}),
     create: (data) => apiService.post(urls.movies.movies, data),
-    getById: (id) => apiService.get(`${urls.movies.movies}/${id}`)
+    getById: (id) => apiService.get(urls.movies.movies.byId(id))
 }
 
 export {
