b67320d839f2eeb18a23e0afbf279da44627da60
Alifoxy <nightfoxy666@gmail.com>
2023-02-21 19:47:29 +0300

    comm1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
-
+import css from './Header.module.css';
 const Header = () => {
     return (
         <div>
-            <div>
+            <div className={css.Header}>
                 <h3>This is header</h3>
             </div>
         </div>
