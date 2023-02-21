02f583c9effd739fa2d35671c7570587d1eeafbc
Alifoxy <nightfoxy666@gmail.com>
2023-02-21 13:56:39 +0300

    comm1

--- src/services/moviesService/getMovies.js
+++ src/services/moviesService/getMovies.js
@@ -1,11 +1,11 @@
-import {axiosRequest} from "../axiosService/axiosConfig";
+import {apiService} from "../axiosService/axiosConfig";
 
 import {urls} from "../../configs/urls";
 
 const getMovies = {
-    getAll: (page=1) => axiosRequest.get(urls.movies.movies, {params:{page}}),
-    create: (data) => axiosRequest.post(urls.movies.movies, data),
-    getById: (id) => axiosRequest.get(`${urls.movies.movies}/${id}`)
+    getAll: (page=1) => apiService.get(urls.movies.movies, {params:{page}}),
+    create: (data) => apiService.post(urls.movies.movies, data),
+    getById: (id) => apiService.get(`${urls.movies.movies}/${id}`)
 }
 
 export {
