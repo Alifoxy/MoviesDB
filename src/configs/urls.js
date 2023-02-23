1357b448e16f5d8104ec58f4d859d66b27bcb279
Alifoxy <nightfoxy666@gmail.com>
2023-02-23 11:21:01 +0300

    comm1

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -1,4 +1,4 @@
-const baseURL ="https://api.themoviedb.org/3/movie/550?api_key=cb1f2b7412e28a08ff00da197875b432&language=en-US&append_to_response=credits"
+const baseURL ="https://api.themoviedb.org/3/movies/page=100?api_key=cb1f2b7412e28a08ff00da197875b432"
 
 const movies = '/movies'
 
